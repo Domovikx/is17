8474499fc12ee361797e9d819f3c230ecb2607b3
Ilya Ivanouski <domovikx@gmail.com>
2019-05-16 12:18:18 +0300

    feat: add laba 2 task 5, 6, 7 are ready

--- oop/java2/Labas/Laba2task4/src/laba2task4/Laba2task4.java
+++ oop/java2/Labas/Laba2task4/src/laba2task4/Laba2task4.java
@@ -6,7 +6,11 @@
 package laba2task4;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -27,8 +31,30 @@ public class Laba2task4 {
          */
         List<Integer> arrayListInt = arrayListInt(25, 0, 9);
         System.out.println(arrayListInt);
+
         List<Integer> changeMaxToMin = changeMaxToMin(arrayListInt);
         System.out.println(changeMaxToMin);
+
+        /*
+        5. Дан массив из целых чисел B(n), где n=1,25. 
+        Необходимо упорядочить массив по возрастанию
+         */
+        List<Integer> sortingArrayListInt = sortingArrayListInt(arrayListInt);
+        System.out.println(sortingArrayListInt);
+
+        /*
+        6. Дан массив из целых чисел С(n), где n=1,20. 
+        Необходимо найти среднее значение и вывести его на консоль.
+         */
+        double averageNumber = averageNumber(arrayListInt);
+        System.out.println(averageNumber);
+
+        /*
+        7. Дан массив из целых чисел D(n), где n=1,30. 
+        Посчитайте сумму четных и нечетных элементов массива.
+         */
+        Map<String, Double> sumOddAndEven = sumOddAndEven(arrayListInt);
+        System.out.println(sumOddAndEven);
     }
 
     public static List<Integer> arrayListInt(int items, int min, int max) {
@@ -68,4 +94,34 @@ public class Laba2task4 {
         return list;
     }
 
+    public static List<Integer> sortingArrayListInt(List<Integer> arrayListInt) {
+        Collections.sort(arrayListInt);
+        return arrayListInt;
+    }
+
+    public static double averageNumber(List<Integer> arrayListInt) {
+        double sum = 0, averageNumber;
+        int quantity = arrayListInt.size();
+        for (Integer item : arrayListInt) {
+            sum += (double) item;
+        }
+        averageNumber = sum / quantity;
+        return averageNumber;
+    }
+
+    public static Map<String, Double> sumOddAndEven(List<Integer> arrayListInt) {
+        double sumOdd = 0, sumEven = 0;
+        Map<String, Double> sumOddAndEven = new HashMap<>();
+
+        for (Integer item : arrayListInt) {
+            if (item % 2 == 0) {
+                sumOdd += item;
+            } else {
+                sumEven += item;
+            }
+        }
+        sumOddAndEven.put("Сумма четных элементов ", sumOdd);
+        sumOddAndEven.put("Сумма нечетных элементов ", sumEven);
+        return sumOddAndEven;
+    }
 }
