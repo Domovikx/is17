92ff338e78a28b850e00fd387ca33f925358758c
Ilya Ivanouski <domovikx@gmail.com>
2019-05-24 12:00:32 +0300

    calc init

---
+++ WEB/calculator/script.js
@@ -0,0 +1,12 @@
+﻿// function sum() {
+// 	let str = document.getElementById('str').value;
+// 	let result = eval(str);
+// 	console.log(result);
+// 	document.getElementById('result').innerHTML = result;
+// }
+
+// document.getElementById('select').addEventListener("change", (e) => {
+// 	let val = e.target.value;
+// 	document.getElementById('figure-circle').style.backgroundColor = val;
+// });
+
